31202701300b5ce67be2a66d49da763062d923b8
Dima Snisarenko <snisarenkodima@gmail.com>
2021-06-20 11:44:24 +0200

    chore: ts project structure

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+import { existsSync } from 'fs';
+
+const currentDir = process.cwd();
+const packagePath = `${currentDir}/package.json`;
+
+if (!existsSync(packagePath)) {
+    console.error(`package.json can't be found in ${currentDir}`);
+}
